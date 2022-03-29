46834f85d4d791b7e80ec00a32b7e15704f2be75
Sanmiri11 <msanchezm1502@alumno.ipn.mx>
2022-03-29 16:00:29 -0600

    modifique uri en bd

--- config/bd.py
+++ config/bd.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
 
-mongo_url ='mongodb://mongo:iPAnHlBPYNyPlsoIltgc@containers-us-west-15.railway.app:7323'
+mongo_url ='mongodb+srv://mirimiri11:<password>@cluster0.gzt7z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
 conn = MongoClient(mongo_url)
